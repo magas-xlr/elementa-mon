5a0ed6751e2e19e818951e214c46ec38cc47265e
Johnny Silva <johnnysilva.dev@gmail.com>
2024-11-26 18:48:41 -0300

    feat(EM02): creating mfe-remote battle

--- apps/frontend/battle/jest.config.ts
+++ apps/frontend/battle/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
   displayName: 'battle',
-  preset: '../jest.preset.js',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../coverage/battle',
+  coverageDirectory: '../../../coverage/apps/frontend/battle',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
--- apps/frontend/host/src/app/app.routes.ts
+++ apps/frontend/host/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
+    path: 'battle',
+    loadChildren: () => import('battle/Routes').then((m) => m!.remoteRoutes),
+  },
+  {
     path: 'avatar',
     loadChildren: () => import('avatar/Routes').then((m) => m!.remoteRoutes),
   },
