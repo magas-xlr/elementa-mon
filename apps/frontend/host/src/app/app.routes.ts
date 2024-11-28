faffc698d79708f4f3f25b762b58508960155e22
Johnny Silva <johnnysilva.dev@gmail.com>
2024-11-28 14:13:13 -0300

    refactor(EM02): add dynamic module federation

--- apps/frontend/host/src/app/app.routes.ts
+++ apps/frontend/host/src/app/app.routes.ts
@@ -1,12 +1,21 @@
 import { Route } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 export const appRoutes: Route[] = [
   {
-    path: 'battle',
-    loadChildren: () => import('battle/Routes').then((m) => m!.remoteRoutes),
+    path: 'avatar',
+    loadChildren: () =>
+      loadRemoteModule('avatar', './Routes').then((m) => m.remoteRoutes),
   },
   {
-    path: 'avatar',
-    loadChildren: () => import('avatar/Routes').then((m) => m!.remoteRoutes),
+    path: 'battle',
+    loadChildren: () =>
+      loadRemoteModule('battle', './Routes').then((m) => m.remoteRoutes),
   },
+  {
+    path: 'deck',
+    loadChildren: () =>
+      loadRemoteModule('deck', './Routes').then((m) => m.remoteRoutes),
+  },
+
 ];
--- apps/frontend/host/src/main.ts
+++ apps/frontend/host/src/main.ts
@@ -1,1 +1,8 @@
-import('./bootstrap').catch((err) => console.error(err));
+// import('./bootstrap').catch((err) => console.error(err));
+
+import { loadManifest } from '@angular-architects/module-federation';
+
+ loadManifest("/module-federation.manifest.json")
+   .catch(err => console.error(err))
+   .then(_ => import('./bootstrap'))
+   .catch(err => console.error(err));
