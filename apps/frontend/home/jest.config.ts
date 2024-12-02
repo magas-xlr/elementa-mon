71fc542c2d4b2c616c8ca8110b77e08aaddce0c8
Johnny Silva <johnnysilva.dev@gmail.com>
2024-12-02 13:20:55 -0300

    feat(EM02): creating mfe-remote home

--- apps/frontend/home/jest.config.ts
+++ apps/frontend/home/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
   displayName: 'home',
-  preset: '../jest.preset.js',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../coverage/home',
+  coverageDirectory: '../../../coverage/apps/frontend/home',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
