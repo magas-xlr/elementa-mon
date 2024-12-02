b1750f45722f2d1021ef2b7bbbb242cb60eeb5b0
Johnny Silva <johnnysilva.dev@gmail.com>
2024-12-02 13:22:44 -0300

    refactor(EM2): changing mfe loads to lazyLoading

--- apps/frontend/host/src/app/app.routes.ts
+++ apps/frontend/host/src/app/app.routes.ts
@@ -1,29 +1,78 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Route } from '@angular/router';
-import { loadRemote } from '@module-federation/enhanced/runtime';
+// import { loadRemote } from '@module-federation/enhanced/runtime';
 
 export const appRoutes: Route[] = [
-  {
+
+{
     path: 'avatar',
     loadChildren: () =>
-      loadRemote<typeof import('avatar/Routes')>('avatar/Routes').then(
+      loadRemoteModule('avatar', './Routes').then(
         (m) => m!.remoteRoutes
       ),
   },
   {
     path: 'battle',
     loadChildren: () =>
-      loadRemote<typeof import('battle/Routes')>('battle/Routes').then((m) => m!.remoteRoutes),
+      loadRemoteModule('battle', './Routes').then(
+        (m) => m!.remoteRoutes
+      ),
   },
   {
     path: 'deck',
     loadChildren: () =>
-      loadRemote<typeof import('deck/Routes')>('deck/Routes').then((m) => m!.remoteRoutes),
+      loadRemoteModule('deck', './Routes').then(
+        (m) => m!.remoteRoutes
+      ),
   },
   {
     path: 'game-over',
     loadChildren: () =>
-      loadRemote<typeof import('gameOver/Routes')>('gameOver/Routes').then(
+      loadRemoteModule('gameOver', './Routes').then(
         (m) => m!.remoteRoutes
       ),
   },
+  {
+    path: 'home',
+    loadChildren: () =>
+      loadRemoteModule('home', './Routes').then(
+        (m) => m!.remoteRoutes
+      ),
+  },
+
+  // {
+  //   path: 'avatar',
+  //   loadChildren: () =>
+  //     loadRemote<typeof import('avatar/Routes')>('avatar/Routes').then(
+  //       (m) => m!.remoteRoutes
+  //     ),
+  // },
+  // {
+  //   path: 'battle',
+  //   loadChildren: () =>
+  //     loadRemote<typeof import('battle/Routes')>('battle/Routes').then(
+  //       (m) => m!.remoteRoutes
+  //     ),
+  // },
+  // {
+  //   path: 'deck',
+  //   loadChildren: () =>
+  //     loadRemote<typeof import('deck/Routes')>('deck/Routes').then(
+  //       (m) => m!.remoteRoutes
+  //     ),
+  // },
+  // {
+  //   path: 'game-over',
+  //   loadChildren: () =>
+  //     loadRemote<typeof import('gameOver/Routes')>('gameOver/Routes').then(
+  //       (m) => m!.remoteRoutes
+  //     ),
+  // },
+  // {
+  //   path: 'home',
+  //   loadChildren: () =>
+  //     loadRemote<typeof import('home/Routes')>('home/Routes').then(
+  //       (m) => m!.remoteRoutes
+  //     ),
+  // },
 ];
